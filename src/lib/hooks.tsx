49c58ce364d702842d62b083bf24288e474a13df
cherrydub <cherrydub@gmail.com>
2024-03-21 00:20:27 +0000

    tidy up and adding types for each component prop

--- src/lib/hooks.tsx
+++ src/lib/hooks.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+import { JobItem } from "./types";
 
 export function useJobItems(searchText: string) {
-  const [jobItems, setJobItems] = useState([]);
+  const [jobItems, setJobItems] = useState<JobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const jobItemsSliced = jobItems.slice(0, 7);
 
   useEffect(() => {
     if (!searchText) return;
@@ -19,7 +22,7 @@ export function useJobItems(searchText: string) {
   }, [searchText]);
 
   return {
-    jobItems,
+    jobItemsSliced,
     isLoading,
   };
 }
