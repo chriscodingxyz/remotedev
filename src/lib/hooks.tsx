53690b7d264ab6538236fd1f4825ab6c92ddac99
cherrydub <cherrydub@gmail.com>
2024-03-23 10:58:52 +0000

    refactored single job to use tanstack, pulled async function outside of the main hook, will now update search fetch to tanstack

--- src/lib/hooks.tsx
+++ src/lib/hooks.tsx
@@ -3,6 +3,17 @@ import { JobItem, JobItemExpanded } from "./types";
 import { BASE_API_URL } from "./constants";
 import { useQuery } from "@tanstack/react-query";
 
+const fetchJobItem = async (id: number): Promise<JobItemExpanded> => {
+  const response = await fetch(`${BASE_API_URL}/${id}`);
+  //4xx or 5xx error, browser may still show 200
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.description || "Something went wrong");
+  }
+  const data = await response.json();
+  return data.jobItem;
+};
+
 export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState<JobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -31,23 +42,22 @@ export function useJobItems(searchText: string) {
 }
 
 export function useJobItem(id: number | null) {
-  const { data, isLoading } = useQuery(
+  const { data, isInitialLoading } = useQuery(
     ["job-item", id],
-    async () => {
-      if (!id) return null;
-      const response = await fetch(`${BASE_API_URL}/${id}`);
-      const data = await response.json();
-      return data.jobItem;
-    },
+    () => fetchJobItem(id as number),
     {
       staleTime: 1000 * 60 * 5,
       refetchOnWindowFocus: false,
       retry: false,
       enabled: Boolean(id),
+      onError: (error) => {
+        console.error(error);
+      },
     }
   );
 
   const jobItem = data as JobItemExpanded;
+  const isLoading = isInitialLoading;
   return { jobItem, isLoading };
 }
 
