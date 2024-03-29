b81007591d36a2e2144b8e38f41ca7868d454957
cherrydub <cherrydub@gmail.com>
2024-03-29 15:16:59 +0000

    very crucial part for the popover, wrapped it in a createPortal so its higher in the dom tree

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,14 +1,16 @@
 import { forwardRef } from "react";
 import { useBookMarksContext } from "../lib/hooks";
 import JobList from "./JobList";
+import { createPortal } from "react-dom";
 
 const BookmarksPopover = forwardRef<HTMLDivElement>(({}, ref) => {
   const { bookmarkedJobItems, isLoading } = useBookMarksContext();
 
-  return (
+  return createPortal(
     <div ref={ref} className="bookmarks-popover">
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
-    </div>
+    </div>,
+    document.body
   );
 });
 
