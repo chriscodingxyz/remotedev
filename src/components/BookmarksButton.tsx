b8d8611096e05c8a49d08db9bc1ed8ad7f693def
cherrydub <cherrydub@gmail.com>
2024-03-30 11:22:21 +0000

    added a length of the bookmarkedIds to add in the bookmarks button to show there are bookmarks there

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,12 +1,13 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import BookmarksPopover from "./BookmarksPopover";
 import { useEffect, useRef, useState } from "react";
-import { useOnClickOutside } from "../lib/hooks";
+import { useBookMarksContext, useOnClickOutside } from "../lib/hooks";
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
   const popoverRef = useRef<HTMLDivElement>(null);
+  const { bookmarkedIds } = useBookMarksContext();
 
   useOnClickOutside([buttonRef, popoverRef], () => {
     setIsOpen(false);
@@ -27,7 +28,8 @@ export default function BookmarksButton() {
         onClick={handleButtonClick}
         className="bookmarks-btn"
       >
-        Bookmarks <TriangleDownIcon />
+        {bookmarkedIds.length ? bookmarkedIds.length : ""} Bookmarks{" "}
+        <TriangleDownIcon />
       </button>
       {isOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
